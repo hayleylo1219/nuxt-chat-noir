93072ee066abe8908f8257a03769cc6b7aa0af85
LAPTOP-NDFDAOBE\user <hayleylo1219@gmail.com>
2025-02-16 00:13:52 +0800

    test

--- server/api/post/get.js
+++ server/api/post/get.js
@@ -1,14 +1,15 @@
 import prisma from "~/prisma/client.ts"
 
 export default defineEventHandler(async (event) => {
-  const query = getQuery(event)
+  // const query = getQuery(event)
 
-  let posts = await prisma.post.findMany({
-    where: query.user_id ? { authorId: query.user_id } : {},
-    orderBy: { id: 'desc' },
-    include: { likes: true, author: { select: { name: true } } }
-  })
+  // let posts = await prisma.post.findMany({
+  //   where: query.user_id ? { authorId: query.user_id } : {},
+  //   orderBy: { id: 'desc' },
+  //   include: { likes: true, author: { select: { name: true } } }
+  // })
 
-  return posts
+  // return posts
+  return 'Hello World!'
 })
 
