5d15a221ac3a4716b3c44fd9a75610fc427d5949
LAPTOP-NDFDAOBE\user <hayleylo1219@gmail.com>
2025-02-16 00:22:46 +0800

    添加錯誤顯示

--- server/api/post/get.js
+++ server/api/post/get.js
@@ -1,15 +1,21 @@
 import prisma from "~/prisma/client.ts"
 
 export default defineEventHandler(async (event) => {
-  // const query = getQuery(event)
 
-  // let posts = await prisma.post.findMany({
-  //   where: query.user_id ? { authorId: query.user_id } : {},
-  //   orderBy: { id: 'desc' },
-  //   include: { likes: true, author: { select: { name: true } } }
-  // })
+  try {
+    const query = getQuery(event)
+    let posts = await prisma.post.findMany({
+      where: query.user_id ? { authorId: query.user_id } : {},
+      orderBy: { id: 'desc' },
+      include: { likes: true, author: { select: { name: true } } }
+    })
 
-  // return posts
-  return 'Hello World!'
+    return posts
+  } catch (error) {
+    console.error('Prisma Error:', error)
+    return { error: 'Server Error: ' + error.message }
+  }
+
+
 })
 
